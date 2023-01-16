45fa5872c2572628556f7a813c9b4910d8a56a30
Artem <art001.163@gmail.com>
2023-01-16 09:59:40 +0400

    Выполнение ДЗ

---
+++ src/main/java/ru/netology/hw81/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.hw81;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
--- src/main/java/ru/netology/hw81/Main.java
+++ src/main/java/ru/netology/hw81/Main.java
@@ -1,7 +1,16 @@
 package ru.netology.hw81;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args) {
+        Post post = new Post();
+        post.name = "Ivan";
+        post.surname = "Ivanov";
+        post.patronymic = "Ivanovich";
+        post.passport = "3333 № 123456";
+        post.phone = "79991234567";
+        post.subscription = true;
+        post.birthday.day = 15;
+        post.birthday.month = 02;
+        post.birthday.year = 1988;
     }
 }
